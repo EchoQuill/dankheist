a9bbc8b98cf6c043a92c6d90074a9596a4721584
kira <92152439+1010saf@users.noreply.github.com>
2023-07-05 18:26:43 +0530

    Update main.py
    fixed local host thingy

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from colorama import Fore, Back, Style
 
 colorama.init(autoreset=True)
 
+colorama.init(autoreset=True)
+
 print(Fore.LIGHTYELLOW_EX + '''
 d8888b. d88888b d8888b. db      
 88  `8D 88'     88  `8D 88      
@@ -27,69 +29,70 @@ print(Back.GREEN + "made by dimlight. on discord :>")
 print()
 print(Fore.LIGHTGREEN_EX + "---------------------------------")
 bot = 270904126974590976
-class MyClient(discord.Client):
 
-  async def on_ready(self):
-    print(Fore.LIGHTGREEN_EX + "---------------------------------")
-    print(f'Logged on as {self.user}!')
-    print(Fore.LIGHTGREEN_EX + "---------------------------------")
-    global dm
-    dm = self.get_user(bot)
-    #print(dm)
-    #global dm
-    global joined
-    joined = 0
-  async def on_message(self, message):
-    if message.channel.id not in channels:
-      return
-    if message.author.id != 270904126974590976:
-      return
-    try:
-      if message.embeds:
-        for embed in message.embeds:
-          if embed.title is not None and "is starting a bank robbery" in embed.title.lower():
-            print(Fore.GREEN + 'heist')
-            async for cmd in dm.slash_commands(query="deposit"):
-              #print(cmd)
-              await asyncio.sleep(1,3)
-              await cmd(amount="max")
-            async for cmd in dm.slash_commands(query="withdraw"):
-              #print(cmd)
-              await asyncio.sleep(1,3)
-              await cmd(amount="2k")
-            await message.components[0].children[0].click()
-            #joined+=1
-            print(Fore.GREEN + 'joined heist') #{joined} times')
-            continue
-      #elif "hi" in message.content.lower():
-        #print(Fore.GREEN + f"hello {message.author}")
-    except Exception as e:
-      print(Fore.LIGHTRED_EX + "---------------------------------")
-      print(f"An error occurred: {e}")
-      print(Fore.LIGHTRED_EX + "---------------------------------")
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(Fore.LIGHTGREEN_EX + "---------------------------------")
+        print(f'Logged on as {self.user}!')
+        print(Fore.LIGHTGREEN_EX + "---------------------------------")
+        global dm
+        dm = self.get_user(bot)
+        global joined
+        joined = 0
+
+    async def on_message(self, message):
+        if message.channel.id not in channels:
+            return
+        if message.author.id != 270904126974590976:
+            return
+        try:
+            if message.embeds:
+                for embed in message.embeds:
+                    if embed.title is not None and "is starting a bank robbery" in embed.title.lower():
+                        print(Fore.GREEN + 'heist')
+                        async for cmd in dm.slash_commands(query="deposit"):
+                            await asyncio.sleep(1, 3)
+                            await cmd(amount="max")
+                        async for cmd in dm.slash_commands(query="withdraw"):
+                            await asyncio.sleep(1, 3)
+                            await cmd(amount="2k")
+                        await message.components[0].children[0].click()
+                        print(Fore.GREEN + 'joined heist')
+                        continue
+        except Exception as e:
+            print(Fore.LIGHTRED_EX + "---------------------------------")
+            print(f"An error occurred: {e}")
+            print(Fore.LIGHTRED_EX + "---------------------------------")
 
 
 async def run_bot(token):
-  client = MyClient()
-  await client.start(token)
+    client = MyClient()
+    await client.start(token)
+
+
+def run_bot_wrapper(token):
+    asyncio.run(run_bot(token))
 
 
 if __name__ == '__main__':
-  channels = [channel1, channel2]  # Replace with your desired channel IDs, DO NOT PUT "" OR '' 
+    channels = [channel1, channel2]  # Replace with your desired channel IDs
 
-  tokens = [
-    'token1','token2','token3'
-  ]  # Add more tokens as needed, use os.environ['token1'] to hide tokens if using replit or other public hosting services
-#make sure to use commas
-  processes = []
+    tokens = [
+    "token2"
+  , "token1"
+  ]
+    #with open('tokens.txt', 'r') as file:
+        #tokens = [line.strip() for line in file]
 
-  for token in tokens:
-    process = multiprocessing.Process(target=asyncio.run,
-                                      args=(run_bot(token), ))
-    processes.append(process)
+    processes = []
 
-  for process in processes:
-    process.start()
+    for token in tokens:
+        process = multiprocessing.Process(target=run_bot_wrapper, args=(token,))
+        processes.append(process)
 
-  for process in processes:
-    process.join()
+    for process in processes:
+        process.start()
+
+    for process in processes:
+        process.join()
