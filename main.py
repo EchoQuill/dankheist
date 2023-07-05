5e93cef636fb68cbc77317c5d78158ec29212ae8
kira <92152439+1010saf@users.noreply.github.com>
2023-07-05 19:03:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,9 @@ import requests
 from discord.ext import commands
 import os
 import asyncio
-import multiprocessing
+import threading
 import colorama
 from colorama import Fore, Back, Style
-
-colorama.init(autoreset=True)
 
 colorama.init(autoreset=True)
 
@@ -21,8 +19,7 @@ d8888b. d88888b d8888b. db
                            || GRINDER V2 ||  ''')
 print(Back.MAGENTA + "Welcome!")
 print()
-print(Back.RED +
-      "IAM NOT RESPONSIBLE FOR IF YOU GET BANNED USING THIS.")
+print(Back.RED + "IAM NOT RESPONSIBLE FOR IF YOU GET BANNED USING THIS.")
 print()
 print(Back.GREEN + "made by dimlight. on discord :>")
 
@@ -32,67 +29,71 @@ bot = 270904126974590976
 
 
 class MyClient(discord.Client):
-    async def on_ready(self):
-        print(Fore.LIGHTGREEN_EX + "---------------------------------")
-        print(f'Logged on as {self.user}!')
-        print(Fore.LIGHTGREEN_EX + "---------------------------------")
-        global dm
-        dm = self.get_user(bot)
-        global joined
-        joined = 0
 
-    async def on_message(self, message):
-        if message.channel.id not in channels:
-            return
-        if message.author.id != 270904126974590976:
-            return
-        try:
-            if message.embeds:
-                for embed in message.embeds:
-                    if embed.title is not None and "is starting a bank robbery" in embed.title.lower():
-                        print(Fore.GREEN + 'heist')
-                        async for cmd in dm.slash_commands(query="deposit"):
-                            await asyncio.sleep(1, 3)
-                            await cmd(amount="max")
-                        async for cmd in dm.slash_commands(query="withdraw"):
-                            await asyncio.sleep(1, 3)
-                            await cmd(amount="2k")
-                        await message.components[0].children[0].click()
-                        print(Fore.GREEN + 'joined heist')
-                        continue
-        except Exception as e:
-            print(Fore.LIGHTRED_EX + "---------------------------------")
-            print(f"An error occurred: {e}")
-            print(Fore.LIGHTRED_EX + "---------------------------------")
+  async def on_ready(self):
+    print(Fore.LIGHTGREEN_EX + "---------------------------------")
+    print(f'Logged on as {self.user}!')
+    print(Fore.LIGHTGREEN_EX + "---------------------------------")
+    global dm
+    dm = self.get_user(bot)
+    #print(dm)
+    #global dm
+    global joined
+    joined = 0
+
+  async def on_message(self, message):
+    if message.channel.id not in channels:
+      return
+    #if message.author.id != 270904126974590976:
+      #return
+    try:
+      if message.embeds:
+        for embed in message.embeds:
+          if embed.title is not None and "is starting a bank robbery" in embed.title.lower(
+          ):
+            print(Fore.GREEN + 'heist')
+            async for cmd in dm.slash_commands(query="deposit"):
+              #print(cmd)
+              await asyncio.sleep(1, 3)
+              await cmd(amount="max")
+            async for cmd in dm.slash_commands(query="withdraw"):
+              #print(cmd)
+              await asyncio.sleep(1, 3)
+              await cmd(amount="2k")
+            await message.components[0].children[0].click()
+            #joined+=1
+            print(Fore.GREEN + f'joined heist:- {self.user}')  #{joined} times')
+            continue
+      #elif "hi" in message.content.lower():
+        #print(Fore.GREEN + f"hello {message.author} {self.user}")
+    except Exception as e:
+      print(Fore.LIGHTRED_EX + "---------------------------------")
+      print(f"An error occurred: {e}")
+      print(Fore.LIGHTRED_EX + "---------------------------------")
 
 
-async def run_bot(token):
-    client = MyClient()
-    await client.start(token)
-
-
-def run_bot_wrapper(token):
-    asyncio.run(run_bot(token))
+def run_bot(token):
+  client = MyClient()
+  client.run(token)
 
 
 if __name__ == '__main__':
-    channels = [channel1, channel2]  # Replace with your desired channel IDs
+  channels = [
+    chanel1,channel2
+  ]  # Replace with your desired channel IDs
 
-    tokens = [
-    "token2"
-  , "token1"
-  ]
-    #with open('tokens.txt', 'r') as file:
-        #tokens = [line.strip() for line in file]
+  tokens = [
+   "tok1","toke2"
+  ]  # Add more tokens as needed, use os.environ['token1'] to hide tokens if using replit or other public hosting services
 
-    processes = []
+  threads = []
 
-    for token in tokens:
-        process = multiprocessing.Process(target=run_bot_wrapper, args=(token,))
-        processes.append(process)
+  for token in tokens:
+    thread = threading.Thread(target=run_bot, args=(token, ))
+    threads.append(thread)
 
-    for process in processes:
-        process.start()
+  for thread in threads:
+    thread.start()
 
-    for process in processes:
-        process.join()
+  for thread in threads:
+    thread.join()
