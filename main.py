d39deb998dbac5cd1fda54abcf342d023ab37ed9
kira <92152439+1010saf@users.noreply.github.com>
2023-07-20 21:40:14 +0530

    Update main.py
    fix

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import asyncio
 import threading
 import colorama
 from colorama import Fore, Back, Style
+import random
 
 colorama.init(autoreset=True)
 
@@ -34,14 +35,12 @@ class MyClient(discord.Client):
         print(Fore.LIGHTGREEN_EX + "---------------------------------")
         print(f'Logged on as {self.user}!')
         print(Fore.LIGHTGREEN_EX + "---------------------------------")
-        global dm
-        dm = self.get_user(bot)
-        # print(dm)
-        # global dm
-        global joined
-        joined = 0
+        self.dm = self.user
+        self.joined = 0
 
     async def on_message(self, message):
+        #if message.channel.id not in channels:
+            #return
         if message.author.id != 270904126974590976:
             return
         try:
@@ -51,11 +50,11 @@ class MyClient(discord.Client):
                         print(Fore.GREEN + 'heist')
                         async for cmd in dm.slash_commands(query="deposit"):
                             # print(cmd)
-                            await asyncio.sleep(1, 3)
+                            asyncio.sleep(random.randint(1,3))
                             await cmd(amount="max")
                         async for cmd in dm.slash_commands(query="withdraw"):
                             # print(cmd)
-                            await asyncio.sleep(1, 3)
+                            asyncio.sleep(random.randint(1,3))
                             await cmd(amount="2k")
                         await message.components[0].children[0].click()
                         # joined+=1
@@ -63,6 +62,8 @@ class MyClient(discord.Client):
                         continue
             elif "hi" in message.content.lower():
                 print(Fore.GREEN + f"hello {message.author} {self.user}")
+        except asyncio.TimeoutError:
+            print("Task took too long and timed out.")
         except Exception as e:
             print(Fore.LIGHTRED_EX + "---------------------------------")
             print(f"An error occurred: {e}")
@@ -77,11 +78,14 @@ def run_bot(token):
     client.run(token)
 
 
+if __name__ == '__main__':
+    #channels = [
+       # channel-1,channel-2
+    #]  # Replace with your desired channel IDs
 
     tokens = [
-       "token1","token2"
-    ]  # Add more tokens as needed, use os.environ['token1'] to hide tokens if using replit or other public hosting services
-
+        "token1", "token2"
+    ]
     threads = []
 
     for token in tokens:
